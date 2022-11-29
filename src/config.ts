2a00aef5ef699bcd1fbfbb5d817d544bb8e7e097
NatnaelFY <natfiky2020@gmail.com>
2022-11-29 08:03:24 +0300

    serverless

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'brb1o1orm4'
+export const apiEndpoint = 'https://${apiId}.execute-api.us-east-1.amazonaws.com/dev'
+
+export const authConfig = {
+  domain: 'dev-mdr0gppxdaeuqnrs.us.auth0.com',
+  clientId: 'qTPGdA5nmgSYahX04vbpXUR3Rm2iKA0p',
+  callbackUrl: 'http://localhost:3000/callback'
+};
